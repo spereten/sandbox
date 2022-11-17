e347a106eaf82c6a52012df6647044c00b735ce3
Artem <work>
2022-11-17 10:46:06 +0300

    эÑчто-то сделал по докуÑерÑ

--- docker/test-1/public/index.php
+++ docker/test-1/public/index.php
@@ -1,12 +1,12 @@
-<? 
+<?php
+
 try {
     $redis = (new \Redis())->connect('redis');
-    var_dump($redis);
-
-
+    $pdo = (new PDO('mysql:dbname=default;host=mysql', 'default', 'default'));
+    $memcached = (new Memcache());
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), ' ',  date('d.m.Y H:i:s') , "\n";
 } catch (Throwable $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-//phpinfo();
+phpinfo();
